80c630d414d5e90b967381b60d2915a6406dc92a
전재욱 <jetacore9219@gmail.com>
2019-08-16 17:15:42 +0900

    algorithm

---
+++ Java/SWExpertAcademy/difficulty3/src/Solution_7510.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Solution_7510 {
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		int T = sc.nextInt();
+		for (int testCase = 1; testCase <= T; testCase++) {
+			int N = sc.nextInt();
+			int result = 0;
+			while(result == N) {
+				
+			}
+			
+			System.out.println("#"+testCase+" ");
+		}
+	}
+}
