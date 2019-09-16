3137f8e3d1c43c207809b7a93633a17e91eb2b6b
전재욱 <jetacore9219@gmail.com>
2019-09-16 17:53:11 +0900

    .

---
+++ Java/SWExpertAcademy/difficulty6/src/Solution_1263.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Solution_1263 {
+	static BufferedReader br;
+	static BufferedWriter bw;
+	public static void main(String[] args) {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	}
+}
