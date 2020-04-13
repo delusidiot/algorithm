dbb0805733b1a6d12bf852d73393d09dd5b15360
JaeWook Jeon <jetacore9219@gmail.com>
2020-04-14 02:55:59 +0900

    .

---
+++ C/BaekJoon/1_9999/1_9999.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#define MAX 50
+using namespace std;
+int N = 0;
+int M = 0;
+
+char map[MAX][MAX];
+int main()
+{
+    
+    cin >> N;
+    cin >> M;
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
+            cin >> map[i][j];
+        }
+    }
+    cout << N << " " << M << endl;
+    
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
+            cout << map[i][j];
+        }
+        cout << endl;
+    }
+}
